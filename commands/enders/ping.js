9e82937ffe684660a7ae444dc9a852ee1bbbc222
unknown <hvscdev@programmer.net>
2020-08-03 15:58:47 +0100

    Update ping.js

--- commands/enders/ping.js
+++ commands/enders/ping.js
@@ -4,8 +4,8 @@ const { MessageEmbed } = require('discord.js')
 module.exports = class PingCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'lag',
-			aliases: ['check','ms','latency','lagtest'],
+			name: 'ping',
+			aliases: ['check','ms','latency','lagtest','lag'],
 			group: 'enders',
 			memberName: 'ping',
 			description: 'Get the latency between Discord and Xara.',
@@ -20,7 +20,7 @@ module.exports = class PingCommand extends Command {
 			.setDescription('How fast is Xara\'s connection to Discord?')
 			.setThumbnail('https://i.imgur.com/uvHQ7oa.png')
 			.addFields(
-				{ name: 'Latency', value: `My current latency is ${Math.round(client.ws.ping)} ms.` },
+				{ name: 'Latency', value: `My current latency is ${Math.round(this.client.ws.ping)} ms.` },
 				{ name: 'What does latency mean?', value:
 				`Latency (or lag) is the time it takes for a packet to reach the server over the Internet.
 				 It affects how fast you get your responses.` },
